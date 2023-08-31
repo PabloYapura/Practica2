8e070e28c49db67e26c70a1ea151301c360d7dd6
PabloYapura <yapura.pablo89@gmail.com>
2023-08-30 21:22:27 -0300

    test 3

---
+++ app.js
@@ -0,0 +1,11 @@
+let nombre;
+// alert(mensaje);
+
+// let name = "Ilya";
+// alert( `Hola ${1}` );
+// alert( `Hola ${"name"}` );
+// alert( `Hola ${name}` );
+
+nombre = prompt("¿Cual es su nombre?");
+
+ alert(`Hola ${nombre}`); 
